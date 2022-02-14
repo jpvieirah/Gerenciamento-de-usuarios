e53f6cd2c5998a33f292992f327d391241abc41d
jpvieirah <jp201101@gmail.com>
2022-02-14 13:04:52 -0300

    Iniciando o projeto usuarios

---
+++ index.js
@@ -0,0 +1,9 @@
+var nome = document.querySelector("#exampleInputName");
+document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputbirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
+
