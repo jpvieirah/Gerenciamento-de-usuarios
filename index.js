a2b1511166750838e97baf88b6fdc2eec9e73b43
jpvieirah <jp201101@gmail.com>
2022-02-15 09:20:44 -0300

    Trabalhando com IF e ELSE

--- index.js
+++ index.js
@@ -7,3 +7,28 @@ var password = document.querySelector("#exampleInputPassword");
 var photo = document.querySelector("#exampleInputFile");
 var admin = document.querySelector("#exampleInputAdmin");
 
+
+
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function(field, index){
+
+    if (field.name == "gender") {
+
+        if (field.checked) {
+
+            console.log("SIM", field);
+        }
+
+       
+
+    } else {
+
+        console.log("NÃO");
+
+    }
+
+//console.log(field.name);
+
+});
